41a28f3799712f2db199a658b737286e7e948ad5
Meina Zhou <meinaz@vmware.com>
2020-04-11 14:59:39 +0800

    Add angular json formatter
    Signed-off-by: Meina Zhou <meinaz@vmware.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,98 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { getHtmlTagDefinition } from '../../node_modules/@angular/compiler';
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'hello-world-angular';
+export class AppComponent implements OnInit {
+  inputText: string;
+  inputObj: object = {
+    "sss": undefined,
+    "aaaa":33,
+    "state": "success",
+    "resources": 
+      {
+        "name": "NSX",
+        "state": "success",
+        "version": "",
+        "resources": 
+      {
+        "name": "NSX",
+        "state": "success",
+        "version": ""
+      }
+      }
+  };
+
+  ngOnInit() {
+    this.bindToggleActions();
+  }
+
+  getObjLength(obj): number {
+    if (obj) {
+      return Object.keys(obj).length;
+    } else {
+      return 0;
+    }
+  }
+
+  convert(): void {
+    this.inputObj = null;
+    if (!this.inputText) {
+      return;
+    } else if (!this.isJsonStr(this.inputText)) {
+      alert('Not valid JSON');
+      return;
+    }
+    this.inputObj = JSON.parse(this.inputText);
+    this.bindToggleActions();
+  }
+
+  bindToggleActions() {
+    $(document).ready(function () {
+      $('.minus').click(function () {
+        $(this).hide();
+        $(this).next().removeClass('hide').next().hide();
+      });
+
+      $('.plus').click(function () {
+        $(this).addClass('hide');
+        $(this).prev().show();
+        $(this).next().show();
+      });
+    });
+  }
+
+  isString(value) {
+    return typeof value === 'string' || value instanceof String;
+  }
+
+  isNumber(value) {
+    return typeof value === 'number' && isFinite(value);
+  }
+
+  isObject(value) {
+    return value && typeof value === 'object' && value.constructor === Object;
+  }
+
+  isArray(value) {
+    return value && typeof value === 'object' && value.constructor === Array;
+  }
+
+  isJsonStr(str): boolean {
+    if (typeof str === 'string') {
+      try {
+        const obj = JSON.parse(str);
+        if (typeof obj === 'object' && obj) {
+          return true;
+        } else {
+          return false;
+        }
+      } catch (e) {
+        return false;
+      }
+    }
+  }
 }
